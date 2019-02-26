edeb7179a09ab7c706a376541402291d299e242e
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-26 15:41:53 +0100

    jams to midi conversion

--- ps_preprocessing.py
+++ ps_preprocessing.py
@@ -188,6 +188,7 @@ def jams_to_midi(filepath, q=1):
 
 def convert_jams_to_midi(folder, q=1):
     for filepath in glob.glob(folder + '/*.jams'):
+        print(filepath)
         filename = filepath.split('/')[-1]
         filename = filename.split('.')[0]
         midi = jams_to_midi(filepath, q)
